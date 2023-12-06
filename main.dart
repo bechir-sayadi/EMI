98cedf3214f956906bdca7d5ca9518c76ca8c441
Der_Markus <57253407+DasMarkus@users.noreply.github.com>
2023-12-06 16:55:37 +0100

    Update main.dart
    added remaining boxes

--- main.dart
+++ main.dart
@@ -28,32 +28,56 @@ class HomePage extends StatelessWidget {
   static const TextStyle heading =
       TextStyle(fontWeight: FontWeight.bold, color: tudBlue, fontSize: 20);
   static const TextStyle normal = TextStyle(color: tudBlue, fontSize: 16);
-  static const TextStyle invert = TextStyle(color: Colors.white, fontSize: 16);
+  static const TextStyle invert = TextStyle(color: Colors.white);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: tudBlue,
         foregroundColor: Colors.white,
         title: Center(child: Text("Mein UNI-Tag")),
         leading: Icon(Icons.abc),
         actions: [
-          DropdownButton(
-            iconDisabledColor: Colors.white,
-            icon: const Icon(Icons.menu),
-            items: [DropdownMenuItem(child: Text(""))],
-            onChanged: (object) {},
-          )
+          IconButton(
+              onPressed: () {
+                //open drawer
+              },
+              icon: Icon(Icons.menu)),
         ],
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(5, 20, 5, 5),
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
           child: Column(
             children: <Widget>[
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   const Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -83,45 +107,254 @@ class HomePage extends StatelessWidget {
                   ), //Uhr
                 ],
               ),
-              Column(
-                children: [
-                  Text(
-                    "Meine Kurse",
-                    style: heading,
-                  ),
-                  Row(
-                    children: [
-                      Column(
+              SizedBox(
+                height: (66 + 15) * 4,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Meine Kurse",
+                      style: heading,
+                      textAlign: TextAlign.left,
+                    ),
+                    SizedBox(
+                      //Erstes der vier Elemente unten
+                      height: 66,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Container(
-                            color: tudBlue,
-                            child: Row(children: [
-                              Icon(Icons.cancel_presentation),
-                              Text("Mo 2. DS\nAPB EOO1", style: invert)
-                            ]),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.21,
+                                height: 32,
+                                color: tudBlue,
+                                child: Row(children: [
+                                  Icon(
+                                    Icons.cancel_presentation,
+                                    color: Colors.white,
+                                  ),
+                                  Text("Mo 2. DS\nAPB EOO1",
+                                      textAlign: TextAlign.right,
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 11))
+                                ]),
+                              ),
+                              Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.21,
+                                  height: 32,
+                                  color: bg,
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.edit),
+                                      Text("Do 09:20",
+                                          textAlign: TextAlign.right)
+                                    ],
+                                  )),
+                            ],
                           ),
                           Container(
-                              color: bg,
-                              child: Row(
-                                children: [Icon(Icons.edit), Text("Do 09:20")],
-                              )),
+                            color: bg,
+                            width: MediaQuery.of(context).size.width * 0.685,
+                            height: 66,
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Algorithmen und\nDatenstrukturen",
+                                    style: normal,
+                                  ),
+                                  GestureDetector(
+                                    child: Icon(Icons.navigate_next),
+                                  )
+                                ]),
+                          )
                         ],
                       ),
-                      Container(
-                        color: bg,
-                        child: Row(children: [
-                          Text(
-                            "Einführung in die \n Mathematik für Informatik",
-                            style: normal,
+                    ),
+                    SizedBox(
+                      //Zweites der vier Elemente unten
+                      height: 66,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.21,
+                                height: 32,
+                                color: tudBlue,
+                                child: Row(children: [
+                                  Icon(
+                                    Icons.cancel_presentation,
+                                    color: Colors.white,
+                                  ),
+                                  Text("Mo 15:30",
+                                      style: invert, textAlign: TextAlign.right)
+                                ]),
+                              ),
+                              Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.21,
+                                  height: 32,
+                                  color: bg,
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.edit),
+                                      Text(
+                                        "Mi 10:50",
+                                        textAlign: TextAlign.right,
+                                      )
+                                    ],
+                                  )),
+                            ],
                           ),
-                          GestureDetector(
-                            child: Icon(Icons.arrow_right),
+                          Container(
+                            color: bg,
+                            width: MediaQuery.of(context).size.width * 0.685,
+                            height: 66,
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Einführung in die \n Mathematik für Informatik",
+                                    style: normal,
+                                  ),
+                                  GestureDetector(
+                                    child: Icon(Icons.navigate_next),
+                                  )
+                                ]),
                           )
-                        ]),
-                      )
-                    ],
-                  )
-                ],
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      //Drittes der vier Elemente unten
+                      height: 66,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.21,
+                                height: 32,
+                                color: bg,
+                                child: Row(children: [
+                                  Icon(
+                                    Icons.cancel_presentation,
+                                    color: tudBlue,
+                                  ),
+                                  Text("Di 7:20",
+                                      textAlign: TextAlign.right,
+                                      style: TextStyle(
+                                          color: tudBlue, fontSize: 11))
+                                ]),
+                              ),
+                              Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.21,
+                                  height: 32,
+                                  color: bg,
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.edit),
+                                      Text("Di 09:20",
+                                          textAlign: TextAlign.right)
+                                    ],
+                                  )),
+                            ],
+                          ),
+                          Container(
+                            color: bg,
+                            width: MediaQuery.of(context).size.width * 0.685,
+                            height: 66,
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Einführungspraktikum\nRoboLab",
+                                    style: normal,
+                                  ),
+                                  GestureDetector(
+                                    child: Icon(Icons.navigate_next),
+                                  )
+                                ]),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      //Viertes der vier Elemente unten
+                      height: 66,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.21,
+                                height: 32,
+                                color: bg,
+                                child: Row(children: [
+                                  Icon(
+                                    Icons.cancel_presentation,
+                                    color: tudBlue,
+                                  ),
+                                  Text("Mi 12:50",
+                                      textAlign: TextAlign.right,
+                                      style: TextStyle(
+                                          color: tudBlue, fontSize: 11))
+                                ]),
+                              ),
+                              Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.21,
+                                  height: 32,
+                                  color: tudBlue,
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.edit, color: Colors.white),
+                                      Text(
+                                        "Mo 09:20",
+                                        textAlign: TextAlign.right,
+                                        style: TextStyle(color: Colors.white),
+                                      )
+                                    ],
+                                  )),
+                            ],
+                          ),
+                          Container(
+                            color: bg,
+                            width: MediaQuery.of(context).size.width * 0.685,
+                            height: 66,
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Einführung in die \n Mathematik für Informatik",
+                                    style: normal,
+                                  ),
+                                  GestureDetector(
+                                    child: Icon(Icons.navigate_next),
+                                  )
+                                ]),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
